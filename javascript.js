e1788359e1ed7c459b25a35232775d7715e0bb6f
paras22072004 <parasbiltoria22@gmail.com>
2025-07-12 17:32:04 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// console.log(x);
+// var x=10;
+
+// console.log(y);
+// let y=10;
+
+// {
+//     var a=10;
+//     let b=200;
+//     const c =300;
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log("var i:", i), 1000);
+// }
+
+// for (let j = 0; j < 3; j++) {
+//   setTimeout(() => console.log("let j:", j), 1000);
+// }
+
+
+{
+  var a = 10;
+  let b = 20;
+}
+console.log(a);
+console.log(b);
